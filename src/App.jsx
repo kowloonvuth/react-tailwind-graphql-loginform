be013fbb0a41c93fa5607128620afe2bc147af8a
Rany Virakvuth <ranyvirakvuth@gmail.com>
2024-07-22 01:00:54 +0700

    finished commit

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,54 @@ export default function App(){
             <div className="w-full lg:w-1/2 px-4 mb-16 lg:mb-0">
               <div className="max-w-lg lg:pt-8 2xl:pt-24 lg:pb-8 mx-auto 2xl:mr-0">
                 <h3 className="text-5xl sm:text-6xl text-gray-900 font-bold mb-4">Welcome Back</h3>
-                <p className="text-lg bg-red-600 text-gray-500 mb-15">See out software in action with the demo version</p>
+                <p className="text-lg text-gray-500 mb-5">See out software in action with the demo version</p>
+                <div className="flex flex-wrap mb-6 items-center -mx-2">
+                  <div className="w-full md:w-1/2 px-2 mb-3 md:mb-0">
+                    <a className="inline-flex w-full py-3 px-4 items-center justify-center rounded-full border border-gray-400 transition duration-100" href="#">
+                      <img className="w-5 h-5 mr-3" src="src/images/facebook-app-symbol.png" alt=""/>
+                      <span className="ml-4 text-sm font-semibold text-gray-500">Login with Facebook</span>
+                    </a>
+                  </div>
+                  <div className="w-full md:w-1/2 px-2">
+                    <a className="inline-flex w-full py-3 px-4 items-center justify-center rounded-full border border-gray-400 transition duration-100" href="#">
+                      <img className="w-5 h-5 mr-3" src="src/images/apple-logo.png" alt=""/>
+                      <span className="ml-4 text-sm font-semibold text-gray-500">Login with Apple</span>
+                    </a>
+                  </div>
+                </div>
+                <div className="flex mb-6 items-center">
+                  <div className="w-full h-px bg-gray-300"></div>
+                  <span className="mx-4 text-sm font-semibold text-gray-500">Or</span>
+                  <div className="w-full h-px bg-gray-300"></div>
+                </div>
+                <form action="#">
+                  <div className="mb-6">
+                    <label className="block mb-1.5 text-sm text-gray-900 font-semibold" for="">Email</label>
+                    <input className="w-full py-3 px-4 text-sm text-gray-900 placeholder-gray-400 border border-gray-200 focus:border-purple-500 focus:outline-purple rounded-lg" type="email" placeholder="cambodiaairway@yahoo.com"></input>
+                  </div>
+                  <div className="mb-7">
+                    <div className="relative">
+                    <input className="w-full py-3 px-4 text-sm text-gray-900 placeholder-gray-400 border border-gray-200 focus:border-purple-500 focus:outline-purple-500 rounded-lg" type="password" placeholder="Enter your password"></input>
+                    <button className="absolute top-1/2 right-0 mr-3 transform -translate-y-1/2 inline-block hover:scale-110 transition duration-100">
+                      <img className="w-5 h-5" src="src/images/eye.png" alt="" />
+                    </button>
+                    </div>
+                  </div>
+                  <div className="flex mb-6 items-center">
+                    <input type="checkbox" value="" id=""/>
+                    <label className="ml-2 text-xs text-gray-800" for="">Remember for 30 days</label>
+                  </div>
+                  <button className="relative group block w-full mb-32 py-3 px-5 text-center text-sm font-semibold text-orange-50 bg-orange-500 rounded-full overflow-hidden" type="submit">
+                    <div className="absolute top-0 right-full w-full h-full bg-gray-800 transform group-hover:translate-x-full group-hover:scale-102 transition duration-500"></div>
+                    <span className="relative">Login</span>
+                  </button>
+                  <div className="text-center">
+                    <span className="text-xs font-semibold text-gray-900">
+                      <span>Dont have account?</span>
+                      <a className="inline-block ml-1 text-orange-500 hover:text-orange-700" href="#">Sign up</a>
+                    </span>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
